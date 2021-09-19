bf795d52cfeb85b45abea6763ce1acd1cea5c9ff
Felix Anducho <felix.anducho74@gmail.com>
2021-09-19 17:46:12 -0500

    Add singly linked list with push, pop methods

---
+++ SinglyLinkedList.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    }
+
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("HOLA");
+list.push("ADIOS");
+
+list.pop();
