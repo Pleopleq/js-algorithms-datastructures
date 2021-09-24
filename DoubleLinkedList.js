fb6abd15c514872fa4dd73bc0f9f9658378cfe92
Felix Anducho <felix.anducho74@gmail.com>
2021-09-24 13:16:17 -0500

    Add DoubleLinkedList with push/pop methods

---
+++ DoubleLinkedList.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let node = new Node(value);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return list;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = temp.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+}
+
+const list = new DoubleLinkedList();
+
+list.push(10);
+list.push("middle");
+list.push(20);
