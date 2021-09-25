47cd0889418794782840257e070fc84c8860f8d2
Felix Anducho <felix.anducho74@gmail.com>
2021-09-24 20:53:40 -0500

    Add shift and unshift methods to DoubleLinkedList

--- DoubleLinkedList.js
+++ DoubleLinkedList.js
@@ -43,6 +43,40 @@ class DoubleLinkedList {
     this.length--;
     return temp;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = temp.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    if (!this.head) return undefined;
+    let node = new Node(value);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const list = new DoubleLinkedList();
