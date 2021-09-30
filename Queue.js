1a2bf2d117583c854d17a3d459b22017001f0512
Felix Anducho <felix.anducho74@gmail.com>
2021-09-30 16:35:35 -0500

    Added Queue class with enqueue and dequeue methods

---
+++ Queue.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let node = new Node(value);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return undefined;
+    let temp = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = temp.next;
+    this.size--;
+
+    return temp.value;
+  }
+}
