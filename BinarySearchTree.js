149a047267c2eae84d32e3bc80f1e146070afdcd
Felix Anducho <felix.anducho74@gmail.com>
2021-10-01 16:59:35 -0500

    Added BST class with insert method

---
+++ BinarySearchTree.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (current.right === null) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+var tree = new BinarySearchTree();
+
+tree.insert(20);
+tree.insert(25);
+tree.insert(12);
+tree.insert(6);
+tree.insert(30);
+tree.insert(20);
