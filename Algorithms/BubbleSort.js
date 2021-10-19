8f6b459af5b54df7f8d3ea38e0ed6703e6c9bdc6
Pleopleq <felix.anducho74@gmail.com>
2021-10-18 19:42:24 -0500

    optimize bubbleSort algorith when data is nearly sorted

--- Algorithms/BubbleSort.js
+++ Algorithms/BubbleSort.js
@@ -1,19 +1,23 @@
-const numArr = [23, 12, 24, 3, 18];
+const numArr = [8, 1, 2, 3, 4, 5, 6, 7];
 
 function swap(arr, index1, index2) {
   let temp = arr[index1];
   arr[index1] = arr[index2];
-  arr[index2] = temp; 
+  arr[index2] = temp;
 }
 
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j+1])
+      console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        noSwaps = false;
       }
     }
+    if (noSwaps) return;
   }
   return arr;
 }
