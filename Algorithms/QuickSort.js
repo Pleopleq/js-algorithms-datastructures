ebb8c7427bceee324e30e338a54323f58cc5c3df
Pleopleq <felix.anducho74@gmail.com>
2021-10-26 17:31:00 -0500

    Add quick sort algorithm

---
+++ Algorithms/QuickSort.js
@@ -0,0 +1,31 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+
+  return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr
+}
+
+console.log(quickSort([9, 4, 8, 2, 1, 5, 7, 6, 3]));
