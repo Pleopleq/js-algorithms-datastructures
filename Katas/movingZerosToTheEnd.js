d3a67e9ef57803722d66ac3a2a130f10ea8181fe
Felix Anducho <felix.anducho74@gmail.com>
2022-03-15 20:52:16 -0600

    Add moving zeros to the end kata

---
+++ Katas/movingZerosToTheEnd.js
@@ -0,0 +1,9 @@
+function moveZeros(arr) {
+  let arrZeros = arr.filter((number) => number === 0);
+  let arrWithoutZeros = arr.filter((number) => number !== 0);
+  return arrWithoutZeros.concat(arrZeros);
+}
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
