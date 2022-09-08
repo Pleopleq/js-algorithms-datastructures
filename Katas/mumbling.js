5406b7827ec35beae84b4be78cdbfbcea4688d90
Felix Anducho <felix.anducho74@gmail.com>
2022-09-07 20:49:46 -0500

    Add mumbling kata

---
+++ Katas/mumbling.js
@@ -0,0 +1,17 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  const stringToArr = s.split("");
+
+  return stringToArr
+    .map((element, index) => {
+      return element.toUpperCase() + element.toLowerCase().repeat(index);
+    })
+    .join("-");
+}
