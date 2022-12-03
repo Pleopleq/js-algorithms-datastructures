dd2cbc3aacde90278eae31cf2172aab7c29a217d
Felix Anducho <felix.anducho74@gmail.com>
2022-12-02 19:27:41 -0600

    Finished day 02

--- advent_of_code_2022/day_02/main.js
+++ advent_of_code_2022/day_02/main.js
@@ -1,4 +1,11 @@
-const fs = require("fs")
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n")
 
 const valuesLeft = {
     A: 1,
@@ -12,27 +19,77 @@ const valuesRight = {
     Z: 3,
 }
 
-function rockPaperScissors() {
-    const input = fs.readFileSync("./input.txt", 'utf8') 
-    .replace(/\r/g, "")
-    .trim()
-    .split("\n")
+function calculateFirstPart() {
+    let score = 0
 
     const formattedScore = input.map(score => {
         return [valuesLeft[score[0]], valuesRight[score[2]]]
     })
 
-    const total = formattedScore.map(pair => {
-        if (pair[0] === pair[1]) {
-           return 3 + pair[1]
-        } else if (pair[1] > pair[0] ) {
-           return 6 + pair[1]
-        } else {
-           return pair[1]
+    formattedScore.forEach(pair => {
+        if (pair[1] === valuesRight.X) {
+            score += valuesRight.X;
+
+            if (pair[0] === valuesLeft.A) {score += 3;}
+            if (pair[0] === valuesLeft.C) {score += 6;}
         }
+
+        if (pair[1] === valuesRight.Y) {
+            score += valuesRight.Y;
+
+            if (pair[0] === valuesLeft.B) {score += 3;}
+            if (pair[0] ===valuesLeft.A){ score += 6;}
+        }
+
+        if (pair[1] === valuesRight.Z) {
+            score += valuesRight.Z;
+
+            if (pair[0] === valuesLeft.C) {score += 3;}
+            if (pair[0] === valuesLeft.B) {score += 6;}
+        } 
     })
 
-    console.log("Part one:", total.reduce((a,b) => a + b, 0))
+    return score
+}
+
+function calculateSecondPart () {
+    let score = 0
+
+    const formattedScore = input.map(score => {
+        return [score[0], score[2]]
+    })
+
+    formattedScore.forEach(pair => {
+        if (pair[1] === "X") {
+            if (pair[0] === "A") {score += 3;}
+            if (pair[0] === "B") {score += 1;}
+            if (pair[0] === "C") {score += 2;}
+        }
+
+        if (pair[1] === "Y") {
+            score += 3;
+            if (pair[0] === "A") {score += 1;}
+            if (pair[0] === "B") {score += 2;}
+            if (pair[0] === "C") {score += 3;}
+        } 
+
+        if (pair[1] === "Z") {
+            score += 6
+            if (pair[0] === "A") {score += 2;}
+            if (pair[0] === "B") {score += 3;}
+            if (pair[0] === "C") {score += 1;}
+        }
+    })
+    
+    return score
+}
+
+function rockPaperScissors() {
+    let scorePartOne = calculateFirstPart()
+    let scorePartTwo = calculateSecondPart()
+    
+    console.log("Part one:", scorePartOne)
+    console.log("Part two:", scorePartTwo)
 }
 
 rockPaperScissors()
